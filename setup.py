926bb461d68369dc268259838214284a87e1aa7c
Stefano Rivera <stefano.rivera@yola.com>
2012-06-20 15:27:42 +0200

    install_requires argparse under Python < 2.7

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import setup, find_packages
 
+install_requires = []
+if sys.version_info < (2, 7):
+    install_requires.append('argparse')
+
 setup(
-    name = "yola.configurator",
-    description = "Configuration Mangling Module for DeployConfigs",
-    author = 'Stefano Rivera',
-    author_email = 'stefanor@yola.com',
-    version = "0.1",
-    packages = find_packages(),
-    scripts = ['bin/configurator.py'],
+    name='yola.configurator',
+    description='Configuration Mangling Module for DeployConfigs',
+    author='Stefano Rivera',
+    author_email='stefanor@yola.com',
+    version="0.1",
+    packages=find_packages(),
+    scripts=['bin/configurator.py'],
+    install_requires=install_requires,
 )
