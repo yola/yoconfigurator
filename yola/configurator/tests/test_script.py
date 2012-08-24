b3d44befe9b394b9281da18afbce775fa4f4e360
Stefano Rivera <stefano.rivera@yola.com>
2012-08-24 16:31:39 +0200

    Use parent virtualenv in test_script

--- yola/configurator/tests/test_script.py
+++ yola/configurator/tests/test_script.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 from . import unittest
 
@@ -8,9 +9,13 @@ class TestScript(unittest.TestCase):
     def test_help(self):
         script_path = os.path.join(os.path.dirname(__file__), '..', '..', '..',
                                    'bin', 'configurator.py')
+        env = {
+            'PATH': os.environ['PATH'],
+            'PYTHONPATH': ':'.join(sys.path),
+        }
         p = subprocess.Popen((script_path, '--help'),
                              stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+                             stderr=subprocess.PIPE, env=env)
         out, err = p.communicate()
         self.assertEqual(p.wait(), 0)
         self.assertEqual(err, '')
