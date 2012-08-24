336a3e9ff2c57e657621115b4ee18a2b1fa40610
Stefano Rivera <stefano.rivera@yola.com>
2012-08-24 16:21:48 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Stefano Rivera',
     author_email='stefanor@yola.com',
     url='https://github.com/yola/yola.configurator',
-    version="0.1",
+    version="0.2.0dev",
     namespace_packages=['yola'],
     packages=find_packages(),
     scripts=['bin/configurator.py'],
