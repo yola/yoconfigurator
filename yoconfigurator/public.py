8eb44e03d5e9ebb6cee74c98040620d0ce44393f
Doug Beck <doug@douglasbeck.com>
2014-12-15 16:53:43 -0500

    Make the public data config file python (not JSON)

--- yoconfigurator/public.py
+++ yoconfigurator/public.py
@@ -1,22 +1,25 @@
 """Contains methods for creating creating public configuration."""
-import json
+import imp
 import os
 
 from yoconfigurator.dicts import DotDict
 
 
+def read_public_data_config(fn):
+    """Return the config keys to be published as public."""
+    if not os.path.isfile(fn):
+        return []
+    m = imp.load_source('public-config', fn)
+    return m.public_data
+
+
 def get_public_config(config, app_config_dir, app_name):
     """Return the subset of config that is defined as public."""
-    public_config_source = os.path.join(app_config_dir, 'public-data.json')
-
-    if not os.path.isfile(public_config_source):
-        return False
-
-    with open(public_config_source) as f:
-        public_keys = json.load(f)
-    config = DotDict(config)
+    public_config_fn = os.path.join(app_config_dir, 'public-data.py')
+    public_data_keys = read_public_data_config(public_config_fn)
     public_config = DotDict()
-    for key in public_keys:
+
+    for key in public_data_keys:
         public_config[key] = config[key]
 
     return public_config
--- yoconfigurator/tests/samples/public-config/deploy/configuration/public-data.py
+++ yoconfigurator/tests/samples/public-config/deploy/configuration/public-data.py
@@ -1,4 +1,4 @@
-[
+public_data = [
     "myapp.hello",
     "myapp.some.deeply.nested.value",
     "myapp.oz"
