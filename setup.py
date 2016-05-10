d643fabeb2008287ec3c2a33956bb7630f68661d
James O'Donnell <james@synthasite.com>
2016-05-10 17:18:51 -0400

    add trove classifiers

--- setup.py
+++ setup.py
@@ -14,4 +14,20 @@ setup(
     packages=find_packages(),
     scripts=['bin/configurator.py'],
     test_suite='yoconfigurator.tests',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ]
 )
